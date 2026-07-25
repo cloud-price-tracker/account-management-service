8e66be77dc4785276956552c04a220d5a7e13235
Sean <stierney0505@gmail.com>
2026-07-25 18:50:58 -0500

    Create application using Spring Initializr

---
+++ src/test/java/cpt/api/account_management_service/AccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cpt.api.account_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
